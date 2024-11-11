88f4298938cbec7197a1559ea0dbd219d25250f8
dimonich00 <dimonich00@gmail.com>
2024-11-11 20:13:49 +0700

    day2

---
+++ day2/circle.py
@@ -0,0 +1,6 @@
+from math import pi
+
+radius = 1
+
+print(f"Area of the circle ={pi * radius * radius: .4f}")
+print(f"Circumference of the circle ={2 * pi *radius: .4f}")
---
+++ day2/rectangle.py
@@ -0,0 +1,5 @@
+width = 10
+height = 5
+
+print(f"Area of the rectangle ={width * height}")
+print(f"Circumference of the rectangle ={2*width + 2*height}")
